96c43e7149ac23b7a72d9fcceaf887a61cc28506
blurosies <nadermarieange@gmail.com>
2025-01-21 08:47:42 +0100

    debut tache 2

--- TACHE2/geometrie2d.h
+++ TACHE2/geometrie2d.h
@@ -1,14 +1,37 @@
 
 typedef struct Point_
 {
-	int abscisse;
-    int ordonne;
-
+	double abs,ord;
 } Point;
 
 typedef struct Vecteur_
 {
-	Point a;
-    Point b;
+	double a,b;
+} Vecteur;
 
-} Point;
+// creer un point a partir de deux coordonnées
+Point creer_point(double abs , double ord);
+
+// creer un vecteur a partir de deux points
+Vecteur creer_vecteur (Point a , Point b);
+
+// somme de deux points
+Point somme_point(Point a, Point b);
+
+// somme de deux vecteurs
+Vecteur somme_vecteur(Vecteur a, Vecteur b);
+
+// multiplication d'un point par un réel
+Point mult_reel_p(Point a, double d);
+
+// multiplication d'un vecteur par un réel
+Point mult_reel_v(Point a, double d);
+
+// Produit scalaire de deux vecteur
+Vecteur produit_scalaire(Vecteur a , Vecteur b);
+
+// Norme d'un vecteur
+double norme_vect(Vecteur a);
+
+// distance entre deux points
+double dist_points(Point a, Point b);
